e4cd81a37eb43d747b26c08667ebdba68230382a
pragadikodi@gmail.com <pragadikodi@gmail.com>
2020-03-03 16:38:25 +0530

    Add project files.

---
+++ MobileShopping.Entity/Account.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MobileShopping.Entity
+{
+    public class Account
+    {
+        
+        [Required]
+        [RegularExpression("^[a-zA-Z][a-zA-Z\\s]+$",ErrorMessage ="Not a valid name")]
+        [MinLength(4),MaxLength(25)]
+        public string UserName { get; set; }
+        
+        [Key]
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        //[RegularExpression("^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$", ErrorMessage = "Not Valid EmailId")]
+        [EmailAddress]
+        public string MailId { get; set; }
+       
+        [Required]
+        [MinLength(10)]
+        public string Password { get; set; }
+        
+        public DateTime CreateDate { get; set; }
+
+        public DateTime UpdatedDate { get; set; }
+        public DateTime LastLoginTime { get; set; }
+      
+        [Range(1, 100, ErrorMessage = "Age Should be min 1 and max 100")]
+        public int Age { get; set; }
+   
+        [Required]
+        public string Gender { get; set; }
+      
+        [Required]
+        //[Phone]
+        // [RegularExpression(@"^([789]\d{9})$", ErrorMessage = "Invalid Mobile Number.")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{2})[-. ]?([0-9]{4})[-. ]?([0-9]{3})[-. ]?([0-9]{3})$", ErrorMessage = "Not a valid Phone number")]
+        public long MobileNo { get; set; }
+        public string City { get; set; }
+
+    }
+}
---
+++ MobileShopping/Controllers/AccountController.cs
@@ -0,0 +1,80 @@
+﻿using System.Web.Mvc;
+using MobileShopping.Entity;
+using MobileShopping.Repository;
+using MobileShopping.Models;
+using System;
+
+namespace MobileShopping.Controllers
+{
+    public class AccountController : Controller
+    {
+        AccountRepository accountRepository;
+        public AccountController()
+        {
+            accountRepository = new AccountRepository();
+        }
+        [HttpGet]
+        [ActionName("SignUp")]
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ActionName("SignUp")]
+        public ActionResult SignUp_Post(SignUpModel signUpModel)
+        {
+            //TryUpdateModel(user);
+            if (ModelState.IsValid)
+            {
+                Account user = new Account();
+                user.UserName = signUpModel.UserName;
+                user.UserId = signUpModel.UserId;
+                user.MailId = signUpModel.MailId;
+                user.Password = signUpModel.Password;
+                user.MobileNo = signUpModel.MobileNo;
+                user.CreateDate = signUpModel.CreateDate;
+                user.UpdatedDate = signUpModel.UpdatedDate;
+                user.LastLoginTime = signUpModel.LastLoginTime;
+                user.Gender = signUpModel.Gender;
+                user.Age = signUpModel.Age;
+                user.City = signUpModel.City;
+               // accountRepository.AddUser(user);
+                TempData["Message"] = "User added successfully!!!";
+                return RedirectToAction("Login");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginModel loginModel)
+        {
+            //TryUpdateModel(user);
+            if (ModelState.IsValid)
+            {
+                Account user = new Account();
+                user.MailId = loginModel.MailId;
+                user.Password = loginModel.Password;
+            }
+            return View();
+        }
+       public ActionResult EditUser([Bind(Exclude = "UserId,MailId,CreateDate")] Account user)
+        {
+            user.UserId = AccountRepository.accounts.Find(name => name.UserName == user.UserName).UserId;
+            user.MailId = AccountRepository.accounts.Find(name => name.UserName == user.UserName).MailId;
+            user.CreateDate = AccountRepository.accounts.Find(name => name.UserName == user.UserName).CreateDate;
+            user.UpdatedDate = DateTime.Now;
+            if (ModelState.IsValid)
+            {
+                accountRepository.UpdateUser(user);
+                TempData["Message"] = "User details edited successfully";
+                return RedirectToAction("Display");
+            }
+            return  View(user);
+        }
+    }
+}
